b0a22c667e5536bf13b3dcfd71e8d071c2837af0
nukuutos <nukuutos@gmail.com>
2022-01-17 12:43:18 +1000

    Change .env file for jest tests

--- tests/global.js
+++ tests/global.js
@@ -1,3 +1,3 @@
 const dotenv = require('dotenv');
 
-module.exports = () => dotenv.config({ path: './config/envs/.env.test' });
+module.exports = () => dotenv.config({ path: './config/envs/.env.jest-test' });
