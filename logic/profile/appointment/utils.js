c7c659052f5f929f4137ce8eb1bb4ca035995ee7
nukuutos <nukuutos@gmail.com>
2022-03-10 21:47:53 +1000

    Update 'get formatted appointments' function

--- logic/profile/appointment/utils.js
+++ logic/profile/appointment/utils.js
@@ -1,13 +1,25 @@
 const dayjs = require('dayjs');
 
-const getFormattedAppointments = (daysWithAppointments) => {
-  daysWithAppointments = daysWithAppointments.map(({ date, appointments }) => ({
-    date: dayjs(date).utc().add(1, 'day'), // prevent utc +0.:00
-    appointments: appointments.map(({ date, ...rest }) => {
-      date = dayjs(date).utc().add(1, 'day').toDate();
-      return { ...rest, date };
-    }),
-  }));
+const getFormattedAppointments = (daysWithAppointments = []) => {
+  daysWithAppointments = daysWithAppointments.map(({ date, appointments }) => {
+    const data = {
+      date: dayjs(date).utc().add(1, 'day'), // prevent utc +0.:00
+      appointments,
+    };
+
+    const isDateInAppointment = appointments[0].date;
+
+    if (isDateInAppointment) {
+      appointments = appointments.map(({ date, ...rest }) => {
+        date = dayjs(date).utc().add(1, 'day').toDate();
+        return { ...rest, date };
+      });
+
+      data.appointments = appointments;
+    }
+
+    return data;
+  });
 
   const sortedDaysWithAppointments = daysWithAppointments.sort((a, b) =>
     a.date.isBefore(b.date) ? 1 : -1
