a094760fe919b756f1a58ba08851ea4aa32d9518
nukuutos <nukuutos@gmail.com>
2022-08-16 11:42:23 +1000

    Switch from replica set to standalone mongodb node

--- utils/database/get-uri.js
+++ utils/database/get-uri.js
@@ -1,14 +1,13 @@
 const { PRODUCTION } = require('../../config/environments');
 
-const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE, DB_REPLICA_SET, NODE_ENV } =
-  process.env;
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE, NODE_ENV } = process.env;
 
 const getURI = () => {
   if (NODE_ENV === PRODUCTION) {
     return `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}`;
   }
 
-  return `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}&replicaSet=${DB_REPLICA_SET}`;
+  return `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
 };
 
 module.exports = getURI;
