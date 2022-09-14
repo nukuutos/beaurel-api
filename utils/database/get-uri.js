1cd6dba63c233491c09015ed4bd98eb9e6b4434a
nukuutos <nukuutos@gmail.com>
2022-09-14 22:45:10 +1000

    Update `getURI` function

--- utils/database/get-uri.js
+++ utils/database/get-uri.js
@@ -1,13 +1,6 @@
-const { PRODUCTION } = require('../../config/environments');
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE } = process.env;
 
-const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE, NODE_ENV } = process.env;
-
-const getURI = () => {
-  if (NODE_ENV === PRODUCTION) {
-    return `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}`;
-  }
-
-  return `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
-};
+const getURI = () =>
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
 
 module.exports = getURI;
